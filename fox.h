4bdb213e40b7bb53eca08708a1da9e956c9f8707
Daniel WSL <ghostr27@hotmail.com>
2019-11-28 04:23:56 +0000

    .

--- fox.h
+++ fox.h
@@ -2,5 +2,5 @@
 #include "fox.h"
 #include "object.h"
 
-void FoxTurn(ObjectPointer fox, int** world, int worldHeight, int worldWidth, int currentGen, ObjectPointer worldObjects);
+void FoxTurn(ObjectPointer fox, int** world, int worldHeight, int worldWidth, int currentGen, ObjectPointer worldObjects, int* currentObjectIndex);
 void MoveFox(ObjectPointer fox, ObjectPointer worldObjects, int direction, int eat);
